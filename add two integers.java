0b45acd7fb3e89fea91055b4a14114525347b18e
N-santho <151705988+N-santho@users.noreply.github.com>
2024-08-17 12:44:31 +0530

    Create add two integers.java

---
+++ add two integers.java
@@ -0,0 +1,11 @@
+import java.io.*;
+public class Main
+{
+    public static void main (String arg[])
+    {
+        int x=-5;
+        int y=10;
+        int z=x+y;
+        System.out.println("Sum of "+x+"and"+y+"is"+z);
+    }
+}
